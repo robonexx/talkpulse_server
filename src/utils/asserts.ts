edd16e6968363e02992ba833087298d01aedfa16
robonexx <robertwagar@gmail.com>
2024-01-03 09:42:55 +0100

    removed unused code - utils

--- src/utils/asserts.ts
+++ src/utils/asserts.ts
@@ -3,10 +3,3 @@ export function assertDefined<T>(value: T): asserts value is NonNullable<T> {
         throw Error('Expected value to be defined, but got: ' + value);
     }
 }
-
-function assertString(str: any): asserts str is NonNullable<string> {
-    assertDefined(str)
-    if (typeof str !== 'string') {
-        throw Error('Expected string, got ' + str);
-    }
-}
