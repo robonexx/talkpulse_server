3a21d9c995dfe4bab3454c294cd53680411380e8
robonexx <robertwagar@gmail.com>
2024-01-03 11:57:55 +0100

    email verification and reset html added to email

--- src/services/EmailVerification.ts
+++ src/services/EmailVerification.ts
@@ -18,11 +18,54 @@ export const verifyEmail = async (username: string, email: string, token: string
         const encodedUsername = encodeURIComponent(username);
         const encodedToken = encodeURIComponent(token);
 
+        const verificationLink = `${process.env.BASE_URL}/verify-account/${encodedUsername}/${encodedToken}`;
+
+        const emailHtml = `
+  <html>
+    <head>
+      <style>
+        body {
+          font-family: Arial, sans-serif;
+          padding: 20px;
+          background-color: #f4f4f4;
+        }
+        .container {
+          max-width: 600px;
+          margin: 0 auto;
+          background-color: #fff;
+          padding: 20px;
+          border-radius: 5px;
+          box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+        }
+        h1 {
+          color: #333;
+        }
+        p {
+          color: #555;
+        }
+        a {
+          color: #007BFF;
+          text-decoration: none;
+        }
+      </style>
+    </head>
+    <body>
+      <div class="container">
+        <h1>Hello ${username}!</h1>
+        <p>Please verify your email to gain access to Talk Pulse and activate your account by clicking the link below:</p>
+        <p><a href="${verificationLink}">${verificationLink}</a></p>
+        <p>If you did not request this verification, please ignore this email.</p>
+      </div>
+    </body>
+  </html>
+`;
+
+
         let info = await transporter.sendMail({
             from: process.env.EMAIL_USER,
             to: email,
-            subject: `Hello ${username} please verify your email to gain access to Talk Pulse and activate your account by clicking the link`,
-            html: `${process.env.BASE_URL}/verify-account/${encodedUsername}/${encodedToken}`,
+            subject: `Hello ${username} Verify Your Email - Activate Your Talk Pulse Account`,
+            html: emailHtml,
         }, (error: any, result: any) => {
             if (error) {
                 console.error("Error sending email: ", error);
@@ -38,13 +81,55 @@ export const verifyEmail = async (username: string, email: string, token: string
 export const passwordResetEmail = async (email: string, token: string) => {
     try {
       const encodedEmail = encodeURIComponent(email);
-      const encodedToken = encodeURIComponent(token);
+        const encodedToken = encodeURIComponent(token);
+        
+        const passwordResetLink = `${process.env.BASE_URL}/reset-password/${encodedEmail}/${encodedToken}`;
+
+        const emailHtml = `
+        <html>
+          <head>
+            <style>
+              body {
+                font-family: Arial, sans-serif;
+                padding: 20px;
+                background-color: #f4f4f4;
+              }
+              .container {
+                max-width: 600px;
+                margin: 0 auto;
+                background-color: #fff;
+                padding: 20px;
+                border-radius: 5px;
+                box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+              }
+              h1 {
+                color: #333;
+              }
+              p {
+                color: #555;
+              }
+              a {
+                color: #007BFF;
+                text-decoration: none;
+              }
+            </style>
+          </head>
+          <body>
+          <div class="container">
+            <h1>TalkPulse Password Reset</h1>
+            <p>We received a request to reset your password. To complete the process, please click the link below:</p>
+            <p><a href="${passwordResetLink}">${passwordResetLink}</a></p>
+            <p>If you did not request a password reset, please ignore this email.</p>
+          </div>
+        </body>
+        </html>
+      `;
   
       await transporter.sendMail({
         from: process.env.EMAIL_USER,
         to: email,
         subject: `Password reset request from Talk pulse`,
-        html: `${process.env.BASE_URL}/reset-password/${encodedEmail}/${encodedToken}`,
+        html: emailHtml,
       });
   
       console.log(`Password reset email sent to: ${email}`);
